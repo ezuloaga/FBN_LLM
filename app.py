c8d12535d930d45d9f86180b58f818fcd2d6b2a6
Eli <ezuloaga@users.noreply.github.com>
2025-07-26 16:06:48 -0500

    template upgrades

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def llm_text_prompt():
     llm_server_to_prompt = remote
     user_prompt = "give me a bulleted list of numbers from 1 to 20 and be quick and brief"
     # user_prompt = "what are the benefits to using flask blueprints?"
-    model_to_use="deepseek/deepseek-r1-0528-qwen3-8b"
-    # model_to_use="google/gemma-3-12b"
+    model_to_use="deepseek/deepseek-r1-0528-qwen3-8b" # remote only
+    # model_to_use="google/gemma-3-12b" # local and remote
     result = text_prompt(user_prompt,llm_server_to_prompt,model_to_use)
     return render_template('text_prompt.html', page_title=page_title, result=result, model_to_use=model_to_use, user_prompt=user_prompt, llm_server_to_prompt=llm_server_to_prompt)
 
@@ -38,9 +38,10 @@ def llm_describe_image():
     page_title="FatGPT | Describe an image"
     image_file = "user_uploads/screenshot.png"
     file_name = 'screenshot.png'
-    llm_server_to_prompt = local
+    llm_server_to_prompt = remote
     user_prompt = "please describe what is in this image"
-    model_to_use="qwen2-vl-2b-instruct"
+    # model_to_use="qwen2-vl-2b-instruct # local only
+    model_to_use="google/gemma-3-12b" # local and remote
     result = describe_image_prompt(user_prompt,llm_server_to_prompt,model_to_use,image_file)
     image_for_show = send_from_directory(UPLOAD_FOLDER, file_name)
     return render_template('describe_image.html',image_file=image_file, page_title=page_title, result=result,model_to_use=model_to_use,user_prompt=user_prompt,llm_server_to_prompt=llm_server_to_prompt, image_for_show=image_for_show)
