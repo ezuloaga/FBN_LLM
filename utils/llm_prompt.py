b006e9265c8d8cde88ebe882ed124bc4031349ff
Eli <ezuloaga@users.noreply.github.com>
2025-07-27 23:22:16 -0500

    format

--- utils/llm_prompt.py
+++ utils/llm_prompt.py
@@ -1,15 +1,9 @@
 import lmstudio as lms
 
+
 user_prompt = None
 lm_studio_host = None
 model_to_use = None
-# text_prompt(user_prompt,lm_studio_host,model_to_use)
-# user_prompt = 'how can you help me?'
-# lm_studio_host = "localhost:1234"
-# model_to_use="qwen2-vl-2b-instruct" # local only
-# lm_studio_host = "192.168.1.29:1234"
-# model_to_use = "google/gemma-3-12b"  # local and remote
-# model_to_use="deepseek/deepseek-r1-0528-qwen3-8b" # remote only
 
 
 def text_prompt(user_prompt,lm_studio_host,model_to_use):
@@ -37,4 +31,3 @@ def describe_image_prompt(user_prompt,lm_studio_host,model_to_use,image_file):
     except Exception as e:
         print(e)
         return e
-
--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -2,9 +2,7 @@ import datetime
 import lmstudio as lms
 
 lm_studio_host = None
-# lm_studio_host = "192.168.1.29:1234"
-# lms.configure_default_client(lm_studio_host)
-# print(lms.get_default_client())
+
 
 class RunUtility:
 
@@ -47,58 +45,3 @@ class RunUtility:
         except Exception as e:
             error_msg = str(e)
             return error_msg
-
-
-# TEST
-# RunUtility.list_downloaded_models(lm_studio_host)
-# RunUtility.get_loaded_models(lm_studio_host)
-# RunUtility.get_server_address(lm_studio_host)
-
-
-# PRINT FUCNCTIONS
-# print(RunUtility.get_server_address(lm_studio_host))
-# print(RunUtility.get_loaded_models(lm_studio_host))
-# print(RunUtility.list_downloaded_models(lm_studio_host))
-
-
-# OBJECT TYPES
-# # # print(type(RunUtility.get_server_address(lm_studio_host)))
-# # # print(type(RunUtility.get_loaded_models(lm_studio_host)))
-# # # print(type(RunUtility.list_downloaded_models(lm_studio_host)))
-
-
-
-
-# # get current model
-# def get_current_model():
-#     model = lms.llm()
-#     print(model)
-#     return model
-#     # retrun model
-#     # print('Current model')
-
-# # Unload model
-# def unload_model():
-#     try:
-#         print('Unloading model')
-#         model = lms.llm()
-#         model.unload()
-#         print('model unloaded')
-#     except:
-#         print(check_model())
-
-# # Check model
-# def check_model():
-#     try:
-#         model = lms.llm()
-#         print(model)
-#     except Exception as e:
-#         print(e)
-
-# def load_model():
-#     model = lms.llm("deepseek/deepseek-r1-0528-qwen3-8b")
-#     print(get_current_model())
-#     print(lms.llm)
-
-
-
